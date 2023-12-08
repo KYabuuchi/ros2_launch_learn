be66710f0c4ba355c19e0945448078514a32ec21
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-12-08 14:53:49 +0900

    initial commit
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <rclcpp/rclcpp.hpp>
+
+class Test : public rclcpp::Node
+{
+public:
+  Test() : Node("test")
+  {
+    auto print = [this](const std::string& label) -> void {
+      RCLCPP_INFO_STREAM(get_logger(), label << " " << declare_parameter<std::string>(label, "initial"));
+    };
+
+    print("before1");
+    print("before2");
+    print("after1");
+    print("after2");
+    print("/namespace/param");
+  }
+
+private:
+};
+
+int main(int argc, char* argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<Test>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
